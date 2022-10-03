41d252c9fac19279acf8b0f7027e9e1b98187a6c
kAzec <kazecx@gmail.com>
2022-10-03 22:16:36 +0800

    update boostrap & sync

--- boostrap.sh
+++ boostrap.sh
@@ -2,16 +2,20 @@
 
 # /bin/zsh -c "$(curl -fsSL 'https://raw.githubusercontent.com/kAzec/.kazec/main/boostrap.sh?token=TOKEN_NEEDED')"
 
-DOTFILES="$HOME/.kazec"
-DOTFILES_REPO='git@github.com:kAzec/.kazec.git'
-BREW='/opt/homebrew/bin/brew'
-BREWFILE='https://gist.githubusercontent.com/kAzec/9cb61c9482daae6ac7673047a9df3bf2/raw/Brewfile'
-XCODE='/Applications/Xcode.app'
+export DOTFILES="$HOME/.kazec"
+export DOTFILES_REPO='git@github.com:kAzec/.kazec.git'
+if [[ $(/usr/bin/uname -m) == "arm64" ]]; then
+  export BREW="/opt/homebrew/bin/brew"
+else
+  export BREW="/usr/local/bin/brew"
+fi
+export BREWFILE='https://gist.githubusercontent.com/kAzec/9cb61c9482daae6ac7673047a9df3bf2/raw/Brewfile'
+export XCODE='/Applications/Xcode.app'
 
 alias ring='afplay /System/Library/Sounds/Ping.aiff -v 2'
 alias echo='echo; echo'
 
-if [ ! -d $DOTFILES ]; then
+if [[ ! -d $DOTFILES ]]; then
   echo 'Generating one-time SSH key for cloning dot files...'
   tempkey=$(mktemp)
   ssh-keygen -t ed25519 -C "kazecx@gmail.com" -N '' -f $tempkey
@@ -23,11 +27,11 @@ if [ ! -d $DOTFILES ]; then
 
   read -s -k '?Press any key to continue cloning...'$'\n'
   git -c core.sshCommand="ssh -i $tempkey" clone $DOTFILES_REPO $DOTFILES
-  
+
   echo 'Linking dot files...'
   for src in $(find $DOTFILES ! -name ".*" ! -name $(basename $0) -maxdepth 1); do
     dest="$HOME/.$(basename $src)"
-    [ -e $dest ] && echo "Backing up $dest" && mv $dest $dest.bak
+    [[ -e $dest ]] && echo "Backing up $dest" && mv $dest $dest.bak
     ln -vs $src $dest
   done
 
@@ -38,7 +42,7 @@ fi
 
 function setup_homebrew()
 {
-  if [ ! -f $BREW ]; then
+  if [[ ! -f $BREW ]]; then
     echo 'Installing Homebrew...'
     ring
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -48,7 +52,7 @@ function setup_homebrew()
   echo 'Removing potentially incomplete downloads...'
   brew cleanup -s
 
-  if [ ! $(command -v parallel) ]; then
+  if ! command -v parallel >/dev/null 2>&1; then
     echo 'Installing gnu-parallel...'
     brew install parallel
     echo 'will cite' | parallel --citation > /dev/null 2>&1
@@ -57,14 +61,14 @@ function setup_homebrew()
 
 function setup_fish()
 {
-  if [ ! $(command -v fish) ]; then
+  if ! command -v fish >/dev/null 2>&1; then
     echo 'Installing fish shell...'
     brew install fish
   fi
 
   if ! grep -q fish /etc/shells; then
     echo $(which fish) | sudo tee -a /etc/shells > /dev/null
-    
+
     ring
     chsh -s $(which fish)
   fi
@@ -77,9 +81,18 @@ source `which env_parallel.zsh`
 
 env_parallel --record-env
 
+function setup_rust()
+{
+  echo 'Installing Rust via rustup...'
+
+  if ! command -v rustup >/dev/null 2>&1; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  fi
+}
+
 function setup_xcode()
 {
-  [ -d $XCODE ] && return 0
+  [[ -d $XCODE ]] && return 0
 
   echo 'Install Latest Xcode...'
 
@@ -100,30 +113,31 @@ function setup_brew_apps()
   echo 'Restoring Homebrew Apps...'
 
   $BREW tap Homebrew/bundle
-  curl -fsSL $BREWFILE | /opt/homebrew/bin/brew bundle --file=-
+  curl -fsSL $BREWFILE | $BREW bundle --file=-
 }
+
+echo 'Running multiple commands in parallel...'
+
+printf "
+setup_rust && echo '...Rust OK'
+setup_xcode && echo '...Xcode OK'
+setup_brew_apps && echo '...Homebrew Apps OK'
+" | env_parallel
 
 function setup_misc()
 {
   echo 'Copying SF-Mono fonts...'
-  cp /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/* $HOME/Library/Fonts > /dev/null 2>&1
+  cp /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/* $HOME/Library/Fonts >/dev/null 2>&1
 
-  if [ ! -f /etc/pam.d/sudo.bak ]; then
+  if ! grep -q pam_tid /etc/pam.d/sudo; then
     echo 'Enabling Touch-ID for terminal sudo...'
     ring
-    awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo.new > /dev/null
+    awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo.new >/dev/null
     sudo cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
     sudo mv /etc/pam.d/sudo.new /etc/pam.d/sudo
   fi
 
   $DOTFILES/sync/sync.fish
 }
-
-echo 'Running multiple commands in parallel...'
-
-printf "
-setup_xcode && echo '...Xcode OK'
-setup_brew_apps && echo '...Homebrew Apps OK'
-" | env_parallel
 
 setup_misc && echo '...Misc OK'
--- sync/sync.fish
+++ sync/sync.fish
@@ -1,20 +1,23 @@
 #!/usr/bin/env fish
 
 set base_dir (dirname (status filename))
-set sync_files (find $base_dir -name .syncdest)
+set sync_files $base_dir/**/.syncdest
 set sync_dirs (dirname $sync_files)
 set sync_dst_dirs (cat $sync_files)
 
-for i in (seq (count $sync_files))
-    set -l f $sync_files[$i]
-    set -l src_dir (dirname $f)
-    set -l dst_dir (string replace -r '^~' $HOME (cat $f))
+for sf in $sync_files
+    set -l src_dir (dirname $sf)
+    set -l dst_dir (string replace -r '^~' $HOME (cat $sf))
     printf 'Syncing contents of %s to %s:\n' $src_dir $dst_dir
 
     for src_file in $src_dir/*
         set -l dst_file $dst_dir"/"(basename $src_file)
-        ln -sv $src_file $dst_file
+        if [ -e $dst_file ]
+            echo "$dst_file already exists. Skipping..."
+        else
+            ln -sv $src_file $dst_file
+        end
     end
 
-    echo 'Done...'
+    echo 'Done.'
 end
